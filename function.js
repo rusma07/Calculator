fc306bb2711cdf8bb154ebc77bb45daf5beb0062
RushmaShrestha <135934869+rusma07@users.noreply.github.com>
2024-09-14 18:26:12 +0545

    first commit

---
+++ function.js
@@ -0,0 +1,21 @@
+function clear() {
+    document.getElementById("display").value = "";
+}
+function deletePrevious() {
+    let display = document.getElementById("display").value;
+    document.getElementById("display").value = display.slice(0, -1);
+}
+
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+
+}
+
+function calculate() {
+    let display = document.getElementById("display").value;
+    try {
+        document.getElementById("display").value = eval(display);
+    } catch (e) {
+        document.getElementById("display").value = "Error";
+    }
+}
